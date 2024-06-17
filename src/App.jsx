713986eb5de87c223681c9aea0132814dbdfdfec
John Lai <lai.john.py@gmail.com>
2024-06-17 11:24:32 -0400

    Add todo form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
 import './styles.css';
 
 export default function App() {
-  return <form></form>;
+  return (
+    <>
+      <form className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item" />
+        <button className="btn">Add</button>
+      </form>
+
+      <h1 className="header">Todo List</h1>
+
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
 }
